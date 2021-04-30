a289950f262b4e56ecd16fb83e30d648ceb5d983
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-04-30 22:15:01 +0900

    Add unsafe test cases

--- src/UnitTest/ParserTest.cs
+++ src/UnitTest/ParserTest.cs
@@ -1,11 +1,9 @@
 #nullable enable
 using System;
 using System.Linq;
-using System.IO;
 using Xunit;
 using U8Xml;
 using U8Xml.Internal;
-using System.Text;
 
 namespace UnitTest
 {
@@ -14,143 +12,48 @@ namespace UnitTest
         [Fact]
         public void Parse()
         {
-            static Func<XmlObject> EncodingTest(ReadOnlySpan<byte> xml, Encoding encoding)
-            {
-                var bytes = Encoding.Convert(Encoding.UTF8, encoding, xml.ToArray());
-                var ms = new MemoryStream(bytes);
-                return () => XmlParser.Parse(ms, encoding);
-            }
-
-            static Func<XmlObject>[] GetTestCases(ReadOnlySpan<byte> xml)
-            {
-                var xmlBytes = xml.ToArray();
-                return new Func<XmlObject>[]
-                {
-                    // from ReadOnlySpan<byte>
-                    () => XmlParser.Parse(xmlBytes.ToArray()),
-                    // from string
-                    () => XmlParser.Parse(Encoding.UTF8.GetString(xmlBytes.ToArray())),
-                    // from ReadOnlySpan<char>
-                    () => XmlParser.Parse(Encoding.UTF8.GetString(xmlBytes.ToArray()).AsSpan()),
-                    // from Stream
-                    () => XmlParser.Parse(new MemoryStream(xmlBytes.ToArray())),
-                    // from Stream, fileSizeHint
-                    () =>
-                    {
-                        var ms = new MemoryStream(xmlBytes.ToArray());
-                        return XmlParser.Parse(ms, (int)ms.Length);
-                    },
-                    // from Stream, Encoding
-                    EncodingTest(xmlBytes.ToArray(), Encoding.UTF8),
-                    EncodingTest(xmlBytes.ToArray(), Encoding.Unicode),
-                    EncodingTest(xmlBytes.ToArray(), Encoding.BigEndianUnicode),
-                    EncodingTest(xmlBytes.ToArray(), Encoding.UTF32),
-                };
-            }
-
-            foreach(var func in GetTestCases(Data.Sample1)) {
+            foreach(var func in TestCases.GetTestCases(Data.Sample1)) {
                 using(var xml = func()) {
-                    TestXml(xml);
+                    TestSample1Contents(xml);
                 }
                 AllocationSafety.Ensure();
             }
-
-            foreach(var func in GetTestCases(Data.ErrorSample1)) {
+            foreach(var func in TestCases.GetTestCases(Data.ErrorSample1)) {
                 Assert.Throws<FormatException>(() =>
                 {
-                    using(var xml = func()) {
-                        TestXml(xml);
-                    }
+                    using var xml = func();
                 });
                 AllocationSafety.Ensure();
             }
-            foreach(var func in GetTestCases(Data.ErrorSample2)) {
+            foreach(var func in TestCases.GetTestCases(Data.ErrorSample2)) {
                 Assert.Throws<FormatException>(() =>
                 {
-                    using(var xml = func()) {
-                        TestXml(xml);
-                    }
+                    using var xml = func();
+                });
+                AllocationSafety.Ensure();
+            }
+
+            foreach(var func in TestCases.GetUnsafeTestCases(Data.Sample1)) {
+                using(var xml = func()) {
+                    TestSample1Contents(xml);
+                }
+                AllocationSafety.Ensure();
+            }
+            foreach(var func in TestCases.GetUnsafeTestCases(Data.ErrorSample1)) {
+                Assert.Throws<FormatException>(() =>
+                {
+                    using var xml = func();
+                });
+                AllocationSafety.Ensure();
+            }
+            foreach(var func in TestCases.GetUnsafeTestCases(Data.ErrorSample2)) {
+                Assert.Throws<FormatException>(() =>
+                {
+                    using var xml = func();
                 });
                 AllocationSafety.Ensure();
             }
             return;
-
-            static void TestXml(XmlObject xml)
-            {
-                Assert.NotNull(xml);
-                var declaration = xml.Declaration;
-                Assert.True(declaration.AsRawString() == @"<?xml version=""1.0"" encoding=""UTF-8""?>");
-                Assert.True(declaration.Version.Name == "version");
-                Assert.True(declaration.Version.Value == "1.0");
-                Assert.True(declaration.Encoding.Name == "encoding");
-                Assert.True(declaration.Encoding.Value == "UTF-8");
-
-                var root = xml.Root;
-                Assert.True(root.Name == "あいうえお");
-                Assert.True(root.InnerText.IsEmpty);
-                Assert.True(root.HasAttribute);
-                Assert.True(root.Attributes.Count == 1);
-                Assert.True(root.HasChildren);
-                Assert.True(root.Children.Count == 2);
-
-                // Test attributus enumeration via interface
-                Assert.True(root.Attributes.First().Name == "ほげ");
-                Assert.True(root.Attributes.First().Value == "3");
-                Assert.True(root.Attributes.First() == ("ほげ", "3"));
-
-                // Test attributes enumeration directly
-                foreach(var attr in root.Attributes) {
-                    Assert.True(attr.Name == "ほげ");
-                    Assert.True(attr.Value == "3");
-                    var (name, value) = attr;
-                    Assert.True(name == "ほげ");
-                    Assert.True(value == "3");
-                    break;
-                }
-
-                // Test children enumeration via interface
-                {
-                    var first = root.Children.First();
-                    Assert.True(first.Name == "かきくけこ");
-                    Assert.True(first.InnerText == "さしすせそ");
-                    Assert.True(first.HasAttribute == false);
-                    Assert.True(first.Attributes.Count == 0);
-                    Assert.True(first.HasChildren == false);
-                    Assert.True(first.Children.Count == 0);
-
-                    var second = root.Children.ElementAt(1);
-                    Assert.True(second.Name == "abc");
-                    Assert.True(second.InnerText == "15 / 3 > A && -3 < B");
-                    Assert.True(second.HasAttribute == false);
-                    Assert.True(second.Attributes.Count == 0);
-                    Assert.True(second.HasChildren == false);
-                    Assert.True(second.Children.Count == 0);
-                }
-
-                // Test children enumeration directly
-                {
-                    int i = 0;
-                    foreach(var child in root.Children) {
-                        if(i == 0) {
-                            Assert.True(child.Name == "かきくけこ");
-                            Assert.True(child.InnerText == "さしすせそ");
-                            Assert.True(child.HasAttribute == false);
-                            Assert.True(child.Attributes.Count == 0);
-                            Assert.True(child.HasChildren == false);
-                            Assert.True(child.Children.Count == 0);
-                        }
-                        else if(i == 1) {
-                            Assert.True(child.Name == "abc");
-                            Assert.True(child.InnerText == "15 / 3 > A && -3 < B");
-                            Assert.True(child.HasAttribute == false);
-                            Assert.True(child.Attributes.Count == 0);
-                            Assert.True(child.HasChildren == false);
-                            Assert.True(child.Children.Count == 0);
-                        }
-                        i++;
-                    }
-                }
-            }
         }
 
         [Fact]
@@ -179,6 +82,83 @@ namespace UnitTest
                 Assert.True(NodeInfoComparer.Default.Equals(tree, ans));
             }
             AllocationSafety.Ensure();
+        }
+
+        private static void TestSample1Contents<TXmlObject>(TXmlObject xml) where TXmlObject : IXmlObject
+        {
+            Assert.NotNull(xml);
+            var declaration = xml.Declaration;
+            Assert.True(declaration.AsRawString() == @"<?xml version=""1.0"" encoding=""UTF-8""?>");
+            Assert.True(declaration.Version.Name == "version");
+            Assert.True(declaration.Version.Value == "1.0");
+            Assert.True(declaration.Encoding.Name == "encoding");
+            Assert.True(declaration.Encoding.Value == "UTF-8");
+
+            var root = xml.Root;
+            Assert.True(root.Name == "あいうえお");
+            Assert.True(root.InnerText.IsEmpty);
+            Assert.True(root.HasAttribute);
+            Assert.True(root.Attributes.Count == 1);
+            Assert.True(root.HasChildren);
+            Assert.True(root.Children.Count == 2);
+
+            // Test attributus enumeration via interface
+            Assert.True(root.Attributes.First().Name == "ほげ");
+            Assert.True(root.Attributes.First().Value == "3");
+            Assert.True(root.Attributes.First() == ("ほげ", "3"));
+
+            // Test attributes enumeration directly
+            foreach(var attr in root.Attributes) {
+                Assert.True(attr.Name == "ほげ");
+                Assert.True(attr.Value == "3");
+                var (name, value) = attr;
+                Assert.True(name == "ほげ");
+                Assert.True(value == "3");
+                break;
+            }
+
+            // Test children enumeration via interface
+            {
+                var first = root.Children.First();
+                Assert.True(first.Name == "かきくけこ");
+                Assert.True(first.InnerText == "さしすせそ");
+                Assert.True(first.HasAttribute == false);
+                Assert.True(first.Attributes.Count == 0);
+                Assert.True(first.HasChildren == false);
+                Assert.True(first.Children.Count == 0);
+
+                var second = root.Children.ElementAt(1);
+                Assert.True(second.Name == "abc");
+                Assert.True(second.InnerText == "15 / 3 > A && -3 < B");
+                Assert.True(second.HasAttribute == false);
+                Assert.True(second.Attributes.Count == 0);
+                Assert.True(second.HasChildren == false);
+                Assert.True(second.Children.Count == 0);
+            }
+
+            // Test children enumeration directly
+            {
+                int i = 0;
+                foreach(var child in root.Children) {
+                    if(i == 0) {
+                        Assert.True(child.Name == "かきくけこ");
+                        Assert.True(child.InnerText == "さしすせそ");
+                        Assert.True(child.HasAttribute == false);
+                        Assert.True(child.Attributes.Count == 0);
+                        Assert.True(child.HasChildren == false);
+                        Assert.True(child.Children.Count == 0);
+                    }
+                    else if(i == 1) {
+                        Assert.True(child.Name == "abc");
+                        Assert.True(child.InnerText == "15 / 3 > A && -3 < B");
+                        Assert.True(child.HasAttribute == false);
+                        Assert.True(child.Attributes.Count == 0);
+                        Assert.True(child.HasChildren == false);
+                        Assert.True(child.Children.Count == 0);
+                    }
+                    i++;
+                }
+            }
         }
     }
 }
---
+++ src/UnitTest/TestCases.cs
@@ -0,0 +1,65 @@
+﻿#nullable enable
+using System;
+using System.Linq;
+using System.IO;
+using U8Xml;
+using System.Text;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    internal static class TestCases
+    {
+        public static IEnumerable<Func<XmlObject>> GetTestCases(ReadOnlySpan<byte> xml)
+        {
+            var xmlBytes = xml.ToArray();
+            return new Func<XmlObject>[]
+            {
+                // from ReadOnlySpan<byte>
+                () => XmlParser.Parse(xmlBytes.ToArray()),
+                // from string
+                () => XmlParser.Parse(Encoding.UTF8.GetString(xmlBytes.ToArray())),
+                // from ReadOnlySpan<char>
+                () => XmlParser.Parse(Encoding.UTF8.GetString(xmlBytes.ToArray()).AsSpan()),
+                // from Stream
+                () => XmlParser.Parse(new MemoryStream(xmlBytes.ToArray())),
+                // from Stream, fileSizeHint
+                () =>
+                {
+                    var ms = new MemoryStream(xmlBytes.ToArray());
+                    return XmlParser.Parse(ms, (int)ms.Length);
+                },
+                // from Stream, Encoding
+                ReEncoding(xmlBytes.ToArray(), Encoding.UTF8),
+                ReEncoding(xmlBytes.ToArray(), Encoding.Unicode),
+                ReEncoding(xmlBytes.ToArray(), Encoding.BigEndianUnicode),
+                ReEncoding(xmlBytes.ToArray(), Encoding.UTF32),
+            };
+        }
+
+        public static IEnumerable<Func<XmlObjectUnsafe>> GetUnsafeTestCases(ReadOnlySpan<byte> xml)
+        {
+            var xmlBytes = xml.ToArray();
+            return new Func<XmlObjectUnsafe>[]
+            {
+                // from ReadOnlySpan<byte>
+                () => XmlParserUnsafe.ParseUnsafe(xmlBytes.ToArray()),
+                // from Stream
+                () => XmlParserUnsafe.ParseUnsafe(new MemoryStream(xmlBytes.ToArray())),
+                // from Stream, fileSizeHint
+                () =>
+                {
+                    var ms = new MemoryStream(xmlBytes.ToArray());
+                    return XmlParserUnsafe.ParseUnsafe(ms, (int)ms.Length);
+                },
+            };
+        }
+
+        private static Func<XmlObject> ReEncoding(ReadOnlySpan<byte> xml, Encoding encoding)
+        {
+            var bytes = Encoding.Convert(Encoding.UTF8, encoding, xml.ToArray());
+            var ms = new MemoryStream(bytes);
+            return () => XmlParser.Parse(ms, encoding);
+        }
+    }
+}
