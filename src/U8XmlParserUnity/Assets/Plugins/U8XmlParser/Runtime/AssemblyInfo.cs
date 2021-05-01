2bf985560b2466d30c651aefc73d183f616a1884
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-05-02 00:36:51 +0900

    Fix

--- src/U8XmlParserUnity/Assets/Plugins/U8XmlParser/Runtime/AssemblyInfo.cs
+++ src/U8XmlParserUnity/Assets/Plugins/U8XmlParser/Runtime/AssemblyInfo.cs
@@ -1,3 +1,9 @@
-﻿using System.Runtime.CompilerServices;
+﻿#if UNITY_2018_1_OR_NEWER
+#define IS_UNITY
+#endif
+
+#if !IS_UNITY
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("UnitTest")]
+#endif
