99edb6b92d816d4c0a1abebc5211c54dee574eb9
ikorin24 <sunshinexxab@yahoo.co.jp>
2022-05-20 02:44:38 +0900

    bugfix #32

---
+++ src/UnitTest/CommentTest.cs
@@ -0,0 +1,37 @@
+﻿#nullable enable
+using Xunit;
+using U8Xml;
+
+namespace UnitTest
+{
+    public class CommentTest
+    {
+        [Fact]
+        public void CommentAtEnd()
+        {
+            using var xml = XmlParser.Parse(
+@"<foo></foo>
+<!-- comment -->");
+            Assert.Equal("foo", xml.Root.Name.ToString());
+        }
+
+        [Fact]
+        public void CommentAtHead()
+        {
+            using var xml = XmlParser.Parse(
+@"<!-- comment -->
+<foo></foo>");
+            Assert.Equal("foo", xml.Root.Name.ToString());
+        }
+
+        [Fact]
+        public void CommentInNode()
+        {
+            using var xml = XmlParser.Parse(
+@"<foo>
+<!-- comment -->
+</foo>");
+            Assert.Equal("foo", xml.Root.Name.ToString());
+        }
+    }
+}
